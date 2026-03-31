42e8d750328f2dd6a1d5a502c939b776664c1a39
Ergi Hokja <ergi1hokja@gmail.com>
2026-03-31 23:54:13 +0100

    Fix app for deployment

--- app.py
+++ app.py
@@ -108,8 +108,10 @@ def logout():
     return redirect(url_for("login"))
 
 # --------------------
+import os
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
